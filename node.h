ffb2d6a0c2009f9af427f427fe71567198a78b32
Justsecret123 <50279622+Justsecret123@users.noreply.github.com>
2019-10-08 02:26:02 +0100

    Update node.h

--- node.h
+++ node.h
@@ -1,37 +1,3 @@
-
 //#include<stdio.h>
 //#include<stdlib.h>
 //#include<conio.h>
-
-
-enum node_color { BLACK , RED };
-
-typedef struct Node
-{
-	struct Node* parent; 
-	struct Node* left; 
-	struct Node* right; 
-	enum node_color color;
-	int key;	
-}
-Node;
-
-Node* getParent(Node* node)
-{
-	return node->parent;
-}
-
-Node* getGrandParent(Node* node)
-{
-	
-}
-
-Node* getUncle(Node* node)
-{
-	
-}
-
-Node* getSibling(Node* node)
-{
-	
-}
