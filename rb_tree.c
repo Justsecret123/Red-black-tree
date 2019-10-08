ef909ae21fb7d8cde41a36a3bcad2e6a4969a9de
Justsecret123 <50279622+Justsecret123@users.noreply.github.com>
2019-10-08 01:25:40 +0100

    Update rb_tree.c

--- rb_tree.c
+++ rb_tree.c
@@ -2,37 +2,7 @@
 #include<stdlib.h>
 #include "print.h"
 #include "node.h"
-enum node_color { BLACK , RED };
 
-struct Node
-{
-	struct Node* parent; 
-	struct Node* left; 
-	struct Node* right; 
-	enum node_color color;
-	int key;	
-}
-Node;
-
-struct Node* getParent(struct Node* node)
-{
-	return node->parent;
-}
-
-struct Node* getGrandParent(struct Node* node)
-{
-	
-}
-
-struct Node* getUncle(struct Node* node)
-{
-	
-}
-
-struct Node* getSibling(struct Node* node)
-{
-	
-}
 
 
 int main()
