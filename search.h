bd39cb812f737d83d8d451b54b30e6034729b25f
Justsecret123 <50279622+Justsecret123@users.noreply.github.com>
2019-10-08 02:28:35 +0100

    Update search.h

--- search.h
+++ search.h
@@ -1,2 +1,15 @@
 //#include<stdio.h>
 //#include<stdlib.h>
+
+struct Node* search(struct Node* node, int key)
+{
+	if(node==NULL)
+		{return NULL;}
+	else 
+	{
+		if(node->key == key)
+			{return node;}
+		else
+			{return (search(node->left,key)||search(node->right,key));}
+	}
+}
