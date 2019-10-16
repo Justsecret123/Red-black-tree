fdf1b1a2c8d662f12227046ff5b83a712968a140
Secret123 <50279622+Justsecret123@users.noreply.github.com>
2019-10-16 07:04:23 +0100

    Update node.h

--- node.h
+++ node.h
@@ -1,3 +1,18 @@
 //#include<stdio.h>
 //#include<stdlib.h>
 //#include<conio.h>
+
+Node* newNode(int data)
+{
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->key = data;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = NULL;
+	node->color = "black";
+	
+	return node;	
+}
+
+
+
