0cb7bc5841a3a532094ba7d3453e79fff77ab8fa
Secret123 <50279622+Justsecret123@users.noreply.github.com>
2019-10-16 07:03:55 +0100

    Update display.h

--- display.h
+++ display.h
@@ -1,21 +1,71 @@
 //#include<stdio.h>
 //#include<stdlib.h>
 
- void display_tree(Node* root, int level)
+
+//Buffer size initialization
+void initialize_screen()
+{
+	HANDLE hconsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD buffer;
+	buffer.X = 100;
+	buffer.Y = 100;
+	SetConsoleScreenBufferSize(hconsole,buffer);
+}
+
+
+//Go to the center of the screen
+void goto_center(Node* node)
 {
 	
-	if(root !=NULL)
+	HANDLE hconsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X = 50;
+	pos.Y = 0;
+	SetConsoleCursorPosition(hconsole, pos); //Set cursor at the center of the screen
+	
+	printf(RED "%d", node->key); 
+	printf(RESET "");
+	
+	pos.X = 47;
+	pos.Y = 1;
+	SetConsoleCursorPosition(hconsole, pos);
+	printf("/"); //Display left branch
+	
+	pos.X = 53;
+	pos.Y = 1;
+	SetConsoleCursorPosition(hconsole, pos);
+	printf("\\"); //Display right branch
+	
+}
+
+//Case 1 : Parent's left node
+void goto_left(Node* node)
+{
+	
+}
+
+void goto_right(Node* node)
+{
+	
+}
+
+//Case 2 : Parent's right node
+ void display_tree(Node* node, int level)
+{
+
+	initialize_screen();
+	
+	//Base condition
+	if(node !=NULL)
 		{	
-			char color[5];
-			
-			printf("Node key: %d", root->key);
-			printf("\nColor: %s",root->color);
-			printf("\nLevel: %d", level);
-			printf("\n\n");
-			display_tree(root->left, level+1);
-			display_tree(root->right, level+1);
-			
-		}
-	
-	
+			if(level==0) //Case 0
+				{goto_center(node);}
+			else if(node==node->parent->left) //Case 1
+				{goto_left(node);}
+			else //Case 2
+				{goto_right(node);}
+				
+			display_tree(node->left, level+1);
+			display_tree(node->right, level+1);
+		}	
 }
